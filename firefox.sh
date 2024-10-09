cdd7766b9f3ee45139f33ec460123bc28aeef36f
RVSmooth <itzsubhashis@proton.me>
2024-10-09 08:39:15 +0530

    firefox.sh: Wrap PROFILES var in a function

--- firefox.sh
+++ firefox.sh
@@ -1,43 +1,50 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 FF_PATH="${HOME}/.mozilla"
 FF_INI_PATH="${FF_PATH}/firefox/profiles.ini"
-PROFILES=$(cat ${FF_INI_PATH} | awk -F '=' '/Path/ {print $2}' | tr -d ' ')
 JS_PATH="/tmp/user.js"
 
+source $SCRIPT_DIR/colors.sh
+
+get_profiles() {
+
+  PROFILES=$(cat ${FF_INI_PATH} | awk -F '=' '/Path/ {print $2}' | tr -d ' ')
+
+}
 
 install_js() {
-
+  
+  get_profiles
   for profile in $PROFILES; do
     prof_path="${FF_PATH}/firefox/$profile"
 
-    echo "Installing Betterfox for $prof_path:"
+    ecyan Installing Betterfox for $prof_path
 
     if [ -d "$prof_path" ]; then
       cp -f $JS_PATH $prof_path
     else
-      echo "Directory does not exist: $prof_path"
+      ered Directory does not exist: $prof_path
     fi
-
-    echo
+    donemsg
   done
+    echo
 
 }
 
-create_user() {
-
-  firefox -CreateProfile $(whoami)
-
-}
-
-echo "Grabbing betterfox user.js ...."
+ecyan Grabbing betterfox user.js ....
+sleep 1
 wget -O $JS_PATH https://raw.githubusercontent.com/yokoffing/Betterfox/refs/heads/main/user.js &&
-echo "Done"
+donemsg
 
+sleep 2
+
+ecyan Getting Profiles
+get_profiles
 
 if [ -z "$PROFILES" ]; then
-  echo "No profiles found. Creating a new profile named as $(whoami)"
-  create_user
+  eyellow No profiles found. Creating a new profile named as $(whoami)
+  firefox -CreateProfile $(whoami)
   install_js
 else
   install_js
