57e2eec9b90eafdea3edf39a5cc78bb9c41b84bc
RVSmooth <itzsubhashis@proton.me>
2024-11-09 15:37:15 +0530

    chore: Remove cohesion
    * Web version of Notion works entirely the same as the flatpak version

--- flatpaks.sh
+++ flatpaks.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/colors.sh
 
 emagenta Installing Flatpaks
-flatpak install -y com.notesnook.Notesnook io.github.brunofin.Cohesion
+flatpak install -y com.notesnook.Notesnook 
 donemsg
 
 
