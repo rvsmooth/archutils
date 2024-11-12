92c6630ca49b960cc23f4367129fdbbccd9a2f44
RVSmooth <itzsubhashis@proton.me>
2024-11-12 23:41:02 +0530

    chore: Add iwlib

--- twms.sh
+++ twms.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/colors.sh
 
 emagenta Installing QTile and its dependencies...
-$SPS qtile qtile-extras python-psutil python-dbus-next
+$SPS qtile qtile-extras python-psutil python-dbus-next python-iwlib
 donemsg
 
 emagenta Installing Hyprland and its dependencies....
