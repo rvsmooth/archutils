ef23e98d767ba1723624e8f2aa1d71d117d8e773
RVSmooth <riveeks.smooth@gmail.com>
2024-11-16 23:46:43 +0530

    feat: Set cursor theme for all themes under /usr/share/themes

--- themeing.sh
+++ themeing.sh
@@ -1,26 +1,42 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+THEME_LIST=$(sudo find /usr/share/themes -type f -name "index.theme")
 GTK3_DIR="${HOME}/.config/gtk-3.0"
 source $SCRIPT_DIR/colors.sh
 
 emagenta Installing themeing packages and utils
 $SPS nwg-look bibata-cursor-theme-bin gruvbox-material-gtk-theme-git gruvbox-material-icon-theme-git \
-  ttf-jetbrains-mono-nerd ttf-ubuntu-font-family font-manager papirus-icon-theme ttf-roboto
+  ttf-jetbrains-mono-nerd ttf-ubuntu-font-family font-manager papirus-icon-theme ttf-roboto wget \
+  curl unzip zip 
 
+## get themes
 wget -O /tmp/Dracula.tar.xz https://github.com/dracula/gtk/releases/latest/download/Dracula.tar.xz &&
 	mkdir /tmp/Dracula &&
 	tar -xf /tmp/Dracula.tar.xz -C /tmp/Dracula &&
 	sudo cp -r /tmp/Dracula/* /usr/share/themes 
-
+wget -O /tmp/Catppuccin.zip https://github.com/catppuccin/gtk/releases/download/v1.0.3/catppuccin-mocha-pink-standard+default.zip &&
+	mkdir /tmp/Catppuccin &&
+	unzip /tmp/Catppuccin.zip -d /tmp/Catppuccin &&
+	sudo cp -r /tmp/Catppuccin/* /usr/share/themes
 donemsg
 
 eblue Setting up gtk theme, cursor theme and font...
 mkdir "$GTK3_DIR"
 cp "$SCRIPT_DIR"/assets/settings.ini "$GTK3_DIR"/settings.ini
-sudo sed -i 's/IconTheme=Adwaita/IconTheme=Gruvbox-Material-Dark/g' /usr/share/themes/Gruvbox-Material-Dark/index.theme
-sudo sed -i 's/CursorTheme=Adwaita/CursorTheme=Bibata-Modern-Classic/g' /usr/share/themes/Gruvbox-Material-Dark/index.theme
-sudo sed -i 's/Adwaita/Bibata-Modern-Classic/g' /usr/share/icons/default/index.theme
+
+### A loop to change cursor theme in all existing themes
+for THEMES in $THEME_LIST; do
+	echo $THEMES
+sudo sed -i -e '/^CursorTheme=/d' \
+           -e '/^\[X-GNOME-Metatheme\]/a CursorTheme=Bibata-Modern-Ice' \
+	   "$THEMES"
+cat $THEMES
+echo
+done
+
+#change def cursor theme in root
+sudo sed -i 's/Adwaita/Bibata-Modern-Ice/g' /usr/share/icons/default/index.theme
 donemsg
 
 eblue setting up wallpapers
