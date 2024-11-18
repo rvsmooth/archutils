44b1c934b4fc2fe74a7a9da98cd2cd57add53369
RVSmooth <riveeks.smooth@gmail.com>
2024-11-18 10:05:49 +0530

    chore(dots.sh): Improve version comparison and overall function

--- dots.sh
+++ dots.sh
@@ -4,11 +4,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTS_CLONE_DIR='/tmp/dotfiles'
 LOCAL_DOTS_VERSION="$(cat ${HOME}/.config/assets/version)"
 UPSTREAM_VERSION=$(curl https://gitlab.com/dotsmooth/dotfiles/-/raw/main/.config/assets/version)
-COMPARE=$(vercmp "$LOCAL_DOTS_VERSION" "$UPSTREAM_VERSION")
 BARE_REPO="${HOME}/.dotfiles"
 BARE_REPO_BAK="${HOME}/.dotfiles-bak"
 CONFIGS="${HOME}/.config"
 CONFIGS_BAK="${HOME}/.config-bak"
+LOCAL_DOTS_VINT=$(echo $LOCAL_DOTS_VERSION | tr -d '.')
+UPSTREAM_VINT=$(echo $UPSTREAM_VERSION | tr -d '.')
 
 source $SCRIPT_DIR/colors.sh
 
@@ -26,43 +27,55 @@ function bak_old_repo(){
 	rm -rf $BARE_REPO
 	PDONE
 }
+
 function get_dots(){
 	PYELL Installing dotfiles as a bare git repo
         git clone https://gitlab.com/dotsmooth/dotfiles -b main $DOTS_CLONE_DIR
-	cd $DOTS_CLONE_DIR
-	cp -rf .* * $HOME/ 
+	cd $DOTS_CLONE_DIR || { PRED "Failed to navigate to dotfiles directory."; exit 1; }
+	cp -rf .* * $HOME/ || { PRED "Failed to copy dotfiles."; exit 1; } 
 	cd -
-	mv -f $HOME/.git $BARE_REPO
+	mv -f $HOME/.git $BARE_REPO || { PRED "Failed to move .git directory."; exit 1; }
 	PDONE
 }
 
-function execute_updater(){
-	if [[ "$COMPARE" -lt 0 ]]; then
-		PBLUE Updates found
-		sleep 1
-		echo
-		PYELL Preparing for updates
-		bak_old_repo
-		prepare_env
-		get_dots
-		PDONE
-	elif [[ "$COMPARE" -eq 0 ]]; then
-		PGREEN Already up-to-date
+function update() {
+	bak_old_repo
+	prepare_env
+	get_dots
+}
+
+function fresh_install() {
+	prepare_env
+	get_dots
+}
+
+function check_for_updates() {
+	if [[ "$LOCAL_DOTS_VINT" < "$UPSTREAM_VINT" ]]; then
+		export UPDATE_STATUS="-1"
+	elif [[ "$LOCAL_DOTS_VINT" == "$UPSTREAM_VINT" ]]; then
+		export UPDATE_STATUS="0"
 	fi
 }
 
 if [[ -n "$LOCAL_DOTS_VERSION" ]]; then
+	PYELL Checking installation of dotsmooth 
 	PMAG Dotsmooth is installed already 
 	sleep 1 
 	echo
 	PMAG Checking for Updates
-	execute_updater
+	check_for_updates
+	if [[ "$UPDATE_STATUS" == "-1" ]]; then
+	        PBLUE Updates found 
+		PYELL Updating dotsmooth dotfiles
+		update
+	elif [[ "$UPDATE_STATUS" == "0" ]]; then
+		PGREEN Already Up to date
+	fi
 else
 	PMAG Dotsmooth has not been installed 
 	echo
 	sleep 1
 	PYELL Initiating Fresh installation
 	sleep 1
-	prepare_env
-	get_dots
+	fresh_install
 fi
