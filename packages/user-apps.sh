e748a386c9bea447127326a3b7a02d68e57eb16d
RVSmooth <riveeks.smooth@gmail.com>
2024-12-06 23:39:49 +0530

    chore(packages): Use ungoogled chromium, remove localsend

--- packages/user-apps.sh
+++ packages/user-apps.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR"/../icandy/colors.sh
 
 PMAG Installing User Applications......
-$SPS brave-bin floorp-bin neovim npm gimp thunderbird obs-studio \
-  mpv onlyoffice-bin localsend-bin qbittorrent btop calibre \
+$SPS ungoogled-chromium-bin floorp-bin neovim npm gimp thunderbird obs-studio \
+  mpv onlyoffice-bin qbittorrent btop calibre \
   kiwix-desktop musikcube newsboat 
 
 PDONE
