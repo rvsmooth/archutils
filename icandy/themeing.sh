fc74338da690a900f6a1be09c2298bcfc8e4f6e8
RVSmooth <riveeks.smooth@gmail.com>
2024-12-26 09:20:31 +0530

    chore: Improve theming script

--- icandy/themeing.sh
+++ icandy/themeing.sh
@@ -1,59 +1,57 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-THEME_LIST=$(sudo find /usr/share/themes -type f -name "index.theme")
 GTK3_DIR="${HOME}/.config/gtk-3.0"
 FONTS_CLONE_DIR="/tmp/fonts"
+THEMES_CLONE_DIR="/tmp/themes"
+ICONS_CLONE_DIR="/tmp/icons"
+THEMES_DIR="${HOME}/.themes"
+ICONS_DIR="${HOME}/.icons"
+FONTS_DIR="${HOME}/.fonts"
+WALLS_DIR="${HOME}/Pictures/wallpapers"
+
 source "$SCRIPT_DIR"/../icandy/colors.sh
 
 PMAG Installing themeing packages and utils
-$SPS nwg-look bibata-cursor-theme-bin gruvbox-material-gtk-theme-git gruvbox-material-icon-theme-git \
-  ttf-jetbrains-mono-nerd ttf-ubuntu-font-family font-manager papirus-icon-theme ttf-roboto wget \
+$SPS nwg-look ttf-jetbrains-mono-nerd ttf-ubuntu-font-family font-manager papirus-icon-theme ttf-roboto wget \
   curl unzip zip 
 
-## get themes
-mkdir "$HOME"/.local/share/themes
-wget -O /tmp/Dracula.tar.xz https://github.com/dracula/gtk/releases/latest/download/Dracula.tar.xz &&
-	mkdir /tmp/Dracula &&
-	tar -xf /tmp/Dracula.tar.xz -C /tmp/Dracula &&
-	cp -fr /tmp/Dracula/* "$HOME"/.local/share/themes
-wget -O /tmp/Catppuccin.zip https://github.com/catppuccin/gtk/releases/download/v1.0.3/catppuccin-mocha-pink-standard+default.zip &&
-	mkdir /tmp/Catppuccin &&
-	unzip /tmp/Catppuccin.zip -d /tmp/Catppuccin &&
-	cp -fr /tmp/Catppuccin/* "$HOME"/.local/share/themes
-PDONE
+## get assets
+mkdir -p "$FONTS_CLONE_DIR" "$ICONS_CLONE_DIR" "$THEMES_CLONE_DIR" "$FONTS_DIR" "$ICONS_DIR" "$THEMES_DIR" "$WALLS_DIR"
+
+ASSETS=("fonts" "themes" "icons")
+URLS=(
+    "https://github.com/rvsmooth/Wallpapers/releases/download/1/fonts-ebook.zip"
+    "https://github.com/rvsmooth/Wallpapers/releases/download/1/themes.zip"
+    "https://github.com/rvsmooth/Wallpapers/releases/download/1/icons.zip"
+)
+
+for i in "${!ASSETS[@]}"; do
+    ASSET=${ASSETS[$i]}
+    URL=${URLS[$i]}
+    CLONE_DIR="/tmp/${ASSET}"  
+    DIR="${HOME}/.${ASSET}"
+    
+    PYELL "Downloading $ASSET..."
+    wget -qO "$CLONE_DIR"/"$ASSET".zip "$URL"
+    PDONE
+    PYELL "Setting up $ASSET..."
+    unzip -qo "$CLONE_DIR"/"$ASSET".zip -d "$CLONE_DIR"
+    PDONE
+    rm -f "$CLONE_DIR"/*.zip
+    cp -rf "$CLONE_DIR"/* "$DIR"
+    echo 
+done
 
 PBLUE Setting up gtk theme, cursor theme and font...
 mkdir "$GTK3_DIR"
 cp "$SCRIPT_DIR"/../assets/settings.ini "$GTK3_DIR"/settings.ini
 
-### A loop to change cursor theme in all existing themes
-#for THEMES in $THEME_LIST; do
-#	echo "$THEMES"
-#sudo sed -i -e '/^CursorTheme=/d' \
-#           -e '/^\[X-GNOME-Metatheme\]/a CursorTheme=Bibata-Modern-Ice' \
-#	   "$THEMES"
-#cat "$THEMES"
-#echo
-#done
-
-#change def cursor theme in root
-#sudo sed -i 's/Adwaita/Bibata-Modern-Ice/g' /usr/share/icons/default/index.theme
-#PDONE
-
 PBLUE setting up wallpapers
-mkdir -p ~/Pictures/wallpapers
-git clone https://github.com/rvsmooth/wallpapers ~/Pictures/wallpapers
+git clone https://github.com/rvsmooth/wallpapers "$WALLS_DIR"
 PDONE
 
 PBLUE I really really love candy, do you not?
 sudo sed -i '/\[options\]/a ILoveCandy' /etc/pacman.conf
 PDONE
 
-PYELL Getting fonts
-mkdir -p "$FONTS_CLONE_DIR"
-wget -qO "$FONTS_CLONE_DIR"/fonts.zip https://github.com/rvsmooth/Wallpapers/releases/download/1/fonts-ebook.zip
-unzip -qo "$FONTS_CLONE_DIR"/fonts.zip -d "$FONTS_CLONE_DIR" 
-mkdir -p "$HOME"/.local/share/fonts
-cp -rf "$FONTS_CLONE_DIR"/*.ttf "$HOME"/.local/share/fonts
-PDONE
