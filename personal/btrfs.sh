0f68bd4c3080e859ecaa02242730abd58dc8991a
rvsmooth <riveeks.smooth@gmail.com>
2025-06-23 23:09:50 +0530

    chore: update btrfs.sh
    * better error handeling
    * better functionality
    * tidy up

--- personal/btrfs.sh
+++ personal/btrfs.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-mount /dev/$1 /mnt
+set -e
+
+ROOT="$1"
+BOOT="$2"
+
+error_out() {
+  echo "Error: $1"
+  exit 1
+}
+
+if [[ ! -n "$ROOT" || ! -n "$BOOT" ]]; then
+  echo "Disks are not selected."
+  echo "usage: ./btrfs.sh <root_disk> <boot_disk>"
+  echo "usage: curl -fsSL btrfs.sh | bash -s -- <root_disk> <boot_disk>"
+  echo "example: curl -fsSL btrfs.sh | bash -s -- sda2 sda1"
+  exit 1
+fi
+
+mount /dev/"$ROOT" /mnt || { error_out "Failed to mount /dev/$ROOT to /mnt"; }
 
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
@@ -9,17 +27,16 @@ btrfs subvolume create /mnt/@var_waydroid
 btrfs subvolume create /mnt/@var_libvirt
 btrfs subvolume create /mnt/@snapshots
 
-umount /mnt
+umount -R /mnt || { error_out "Failed to umount /mnt" }
 
-mount -o compress=zstd,noatime,subvol=@ /dev/$1 /mnt/
+mount -o compress=zstd,noatime,subvol=@ /dev/"$ROOT" /mnt/ &&
+  mkdir -p /mnt/{home,var/log,var/cache/pacman/pkg,.snapshots,boot,var/lib/waydroid,var/lib/libvirt}
 
-mkdir -p /mnt/home /mnt/var/log /mnt/var/cache/pacman/pkg /mnt/.snapshots /mnt/boot /mnt/var/lib/waydroid /mnt/var/lib/libvirt
+mount -o compress=zstd,noatime,subvol=@home /dev/"$ROOT" /mnt/home
+mount -o compress=zstd,noatime,subvol=@var_log /dev/"$ROOT" /mnt/var/log
+mount -o compress=zstd,noatime,subvol=@var_cache /dev/"$ROOT" /mnt/var/cache/pacman/pkg
+mount -o compress=zstd,noatime,subvol=@var_waydroid /dev/"$ROOT" /mnt/var/lib/waydroid
+mount -o compress=zstd,noatime,subvol=@var_libvirt /dev/"$ROOT" /mnt/var/lib/libvirt
+mount -o compress=zstd,noatime,subvol=@snapshots /dev/"$ROOT" /mnt/.snapshots
 
-mount -o compress=zstd,noatime,subvol=@home /dev/$1 /mnt/home
-mount -o compress=zstd,noatime,subvol=@var_log /dev/$1 /mnt/var/log
-mount -o compress=zstd,noatime,subvol=@var_cache /dev/$1 /mnt/var/cache/pacman/pkg
-mount -o compress=zstd,noatime,subvol=@var_waydroid /dev/$1 /mnt/var/lib/waydroid
-mount -o compress=zstd,noatime,subvol=@var_libvirt /dev/$1 /mnt/var/lib/libvirt
-mount -o compress=zstd,noatime,subvol=@snapshots /dev/$1 /mnt/.snapshots
-
-mount /dev/$2 /mnt/boot
+mount /dev/"$BOOT" /mnt/boot
