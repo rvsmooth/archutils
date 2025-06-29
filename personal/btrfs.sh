6a62cf1dce006e78c1f78145afd4fd6f37e1ebd1
rvsmooth <riveeks.smooth@gmail.com>
2025-06-29 15:14:01 +0530

    chore: personal: Make btrfs.sh compatible for msdos installations too

--- personal/btrfs.sh
+++ personal/btrfs.sh
@@ -9,11 +9,17 @@ error_out() {
   exit 1
 }
 
-if [[ ! -n "$ROOT" || ! -n "$BOOT" ]]; then
+if [[ ! -n "$ROOT" ]]; then
   echo "Disks are not selected."
+  echo "===================FOR UEFI========================="
   echo "usage: ./btrfs.sh <root_disk> <boot_disk>"
   echo "usage: curl -fsSL btrfs.sh | bash -s -- <root_disk> <boot_disk>"
   echo "example: curl -fsSL btrfs.sh | bash -s -- sda2 sda1"
+  echo ""
+  echo "==================FOR MSDOS========================="
+  echo "usage: ./btrfs.sh <root_disk>"
+  echo "usage: curl -fsSL btrfs.sh | bash -s -- <root_disk>"
+  echo "example: curl -fsSL btrfs.sh | bash -s -- sda2"
   exit 1
 fi
 
@@ -27,10 +33,10 @@ btrfs subvolume create /mnt/@var_waydroid
 btrfs subvolume create /mnt/@var_libvirt
 btrfs subvolume create /mnt/@snapshots
 
-umount -R /mnt || { error_out "Failed to umount /mnt" }
+umount -R /mnt || { error_out "Failed to umount /mnt"; }
 
-mount -o compress=zstd,noatime,subvol=@ /dev/"$ROOT" /mnt/ &&
-  mkdir -p /mnt/{home,var/log,var/cache/pacman/pkg,.snapshots,boot,var/lib/waydroid,var/lib/libvirt}
+mount -o compress=zstd,noatime,subvol=@ /dev/"$ROOT" /mnt/ || { error_out "Failed to mount @ subvolume as /mnt"; }
+mkdir -p /mnt/{home,var/log,var/cache/pacman/pkg,.snapshots,boot,var/lib/waydroid,var/lib/libvirt}
 
 mount -o compress=zstd,noatime,subvol=@home /dev/"$ROOT" /mnt/home
 mount -o compress=zstd,noatime,subvol=@var_log /dev/"$ROOT" /mnt/var/log
@@ -39,4 +45,13 @@ mount -o compress=zstd,noatime,subvol=@var_waydroid /dev/"$ROOT" /mnt/var/lib/wa
 mount -o compress=zstd,noatime,subvol=@var_libvirt /dev/"$ROOT" /mnt/var/lib/libvirt
 mount -o compress=zstd,noatime,subvol=@snapshots /dev/"$ROOT" /mnt/.snapshots
 
-mount /dev/"$BOOT" /mnt/boot
+if [[ -n "$BOOT" ]]; then
+  echo "$BOOT is provided"
+  echo "Mounting $BOOT as /mnt/boot"
+  mount /dev/"$BOOT" /mnt/boot || { error_out "Failed to mount /dev/$BOOT as /boot"; }
+elif [[ ! -n "$BOOT" ]]; then
+  echo "BOOT partition is not provided"
+  echo "Assuming MSDOS installation"
+else
+  echo "$BOOT is unrecognized"
+fi
